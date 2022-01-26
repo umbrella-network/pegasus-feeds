73ed0b751cfce4b37386860d498faab19fc220e8
jjordan-quantum <constant.space@gmail.com>
2022-01-26 13:27:05 -0800

    update feeds.yaml with all symbols from production as a baseline for polygon request threshold

---
+++ src/countFeeds.ts
@@ -0,0 +1,34 @@
+import {
+  getPathToYaml,
+  writeFromFileTo,
+  readFeedsYaml,
+  appendFeedsYaml,
+  getAllSymbols,
+  getEquityYamlDump,
+  getAllFiles,
+} from './utils/feedsUtils';
+/*
+ *  Counts # of symbols with EQ prefix currently in feeds.yaml
+ */
+(async function () {
+  const path = getPathToYaml();
+  const feedsYamlFilePath = path + 'feeds.yaml';
+  const feedYamlData = readFeedsYaml(feedsYamlFilePath);
+  const symbols = Object.keys(feedYamlData);
+  const totalSymbols = symbols.length;
+  let totalEQSymbols = 0
+  symbols.forEach((symbol) => {
+    if(symbol.includes('EQ:')) {
+      totalEQSymbols++;
+    }
+  });
+  console.log("Total symbols: " + totalSymbols);
+  console.log("Total EQ symbols: " + totalEQSymbols);
+})()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
